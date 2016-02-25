ec726a5e68b551105c1d7efdfedf4de40ec7dd4d
adudleyssf <adudley@softstackfactory.org>
2016-02-25 00:56:59 +0000

    finsihed lesson 2

---
+++ www/js/controller.js
@@ -0,0 +1,85 @@
+angular.module('starter.controllers', [])
+.controller('LoginCtrl',['$scope', '$state', 'UserService', '$ionicHistory',
+function($scope, $state, UserService, $ionicHistory){
+    $scope.user = {};
+    $scope.loginSubmitForm = function(form)
+    {
+        if(form.$valid)
+        {  
+           UserService.login($scope.user)
+            .then(function(response) {
+                if (response.status === 200) {
+                    //Should return a token
+                    console.log(response);
+                    $ionicHistory.nextViewOptions({
+                      historyRoot: true,
+                      disableBack: true
+                    });
+                    $state.go('lobby');
+                } else {
+                    // invalid response
+                    alert("Something went wrong, try again.");
+                }
+            }, function(response) {
+                // Code 401 corresponds to Unauthorized access, in this case, the email/password combination was incorrect.
+                if(response.status === 401)
+                {
+                    alert("Incorrect username or password");
+                }else if(response.data === null) {
+//If the data is null, it means there is no internet connection.
+                    alert("The connection with the server was unsuccessful, check your internet connection and try again later.");
+                }else {
+                    alert("Something went wrong, try again.");
+                }
+            });
+
+        }
+    };
+}])
+
+.controller('RegisterCtrl',['$scope', '$state', 'UserService', '$ionicHistory',
+function($scope, $state, UserService, $ionicHistory){
+    $scope.user = {};
+    $scope.comparePassword = {};
+    $scope.registerSubmitForm = function(form)
+    {
+        if(form.$valid)
+        {  
+            if($scope.user.password === $scope.comparePassword.password){
+                
+                
+           UserService.create($scope.user)
+            .then(function(response) {
+                if (response.status === 200) {
+                    //Should return a token
+                    console.log(response);
+                    $ionicHistory.nextViewOptions({
+                      historyRoot: true,
+                      disableBack: true
+                    });
+                    $state.go('lobby');
+                } else {
+                    // invalid response
+                    alert("Something went wrong, try again.");
+                }
+            }, function(response) {
+                // Code 401 corresponds to Unauthorized access, in this case, the email/password combination was incorrect.
+                if(response.status === 401)
+                {
+                    alert("Incorrect username or password");
+                }else if(response.data === null) {
+//If the data is null, it means there is no internet connection.
+                    alert("The connection with the server was unsuccessful, check your internet connection and try again later.");
+                }else if(response.status === 422) {
+                    alert("Email already in use");
+                }else{
+                     alert("Something went wrong");
+                }
+            });
+
+        }
+        }
+    };
+}]);
+
+
