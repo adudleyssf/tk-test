9d8e5c0d5218489f563c71428eb042978f399118
adudleyssf <adudley@softstackfactory.org>
2016-02-26 00:42:51 +0000

    lesson 4 done

--- www/js/TKServices.js
+++ www/js/TKServices.js
@@ -1,23 +1,44 @@
 angular.module('TKServicesModule', [])
-.service('TKQuestionsService', function () {
+    .service('TKQuestionsService', function() {
+        var service = this;
+        var questions = [];
+        service.setQuestions = function(serverQuestions) {
+            questions = serverQuestions;
+        };
+        service.getQuestion = function(questionID) {
+            var results = [];
+            questions.forEach(function(question) {
+                //Search for questions with the specified question ID
+                if (question.Question_Number == questionID)
+                    results.push(question);
+            });
+            return results;
+        };
+        service.questionsLength = function() {
+            return questions.length;
+        };
+    })
+
+.service("TKAnswersService", function() {
     var service = this;
-    var questions = [];
-    service.setQuestions = function(serverQuestions)
-    {
-        questions = serverQuestions;
+    var answerCategories = {
+      
+        "competing": 0,
+        "collaborating": 0,
+        "compromising": 0,
+        "avoiding": 0,
+        "accommodating": 0,
+
     };
-    service.getQuestion = function(questionID)
-    {
-        var results = [];
-        questions.forEach(function(question){
-        //Search for questions with the specified question ID
-            if(question.Question_Number == questionID)
-                results.push(question);
-        });
-        return results;
+    var answers = {};
+
+    service.saveAnswer = function(questionNumber, answerCategory, option) {
+        answerCategories[answerCategory.toLowerCase()]++;
+        answers[questionNumber] = option;
     };
-    service.questionsLength = function()
-    {
-        return questions.length;
+
+    service.getAnswers = function() {
+        return answerCategories;
     };
+
 });
