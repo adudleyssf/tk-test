38b2fc2f79c903a8d8733811950e58e520d4d479
adudleyssf <adudley@softstackfactory.org>
2016-02-29 23:39:32 +0000

    code ship service test 2

--- www/js/SortServices.js
+++ www/js/SortServices.js
@@ -1,6 +1,6 @@
 angular.module('SortServices', [])
     .service('SortArray', function(array) {
-        var sort = array;
+        var sort = [1, 7, 2];
         
-    return array;
+    return sort;
     });
