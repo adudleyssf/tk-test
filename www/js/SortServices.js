0d915fc51dbf5cbace1f552f59430830485cc574
adudleyssf <adudley@softstackfactory.org>
2016-03-01 01:55:01 +0000

    code ship service test 18 count swaps

--- www/js/SortServices.js
+++ www/js/SortServices.js
@@ -1,42 +1,44 @@
 angular.module('SortServices', [])
     .service('sortArrayService', function() {
-            var service = this;
-            var sort = [];
+        var service = this;
+        var sort = [];
+        
+        var obj = new Object(),
+        sorted = array,
+        count = swapCount;
+        
+            
+        
 
-            service.sortArray = function(array) {
-                /*
+        service.sortArray = function(array) {
+            /*
             for(var i = 0; i < array.length; i++){		
 		        if (array[i] > array [i++]){
 		            var temp = array[i];
 		            array.push(temp);
 		        }
-            
-            
              */
-                    var swap = false;
-                do {
-                    swap = false;
-                    for (var i = 0; i < array.length; i++) {
+             var stuff = [];
+            var swap = false;
+            var swapCount = 0;
+            do {
+                swap = false;
+                for (var i = 0; i < array.length; i++) {
 
-                        if (array[i] > array[i++]) {
-                            var temp = array[i];
-                            var temp2 = array[i++];
-                            array[i] = temp2;
-                            array[i++] = temp;
-                            swap = true;
-
-
-                        }
+                    if (array[i] > array[i++]) {
+                        var temp = array[i];
+                        var temp2 = array[i++];
+                        array[i] = temp2;
+                        array[i++] = temp;
+                        swap = true;
+                        swapCount++;
+                        
                     }
-
                 }
-                while (swap);
-                     return array;
-                
-               
 
-
-
+            }
+            while (swap);
+            return array;
             
         };
     });
