cb914b8dc37c4a121c97535c1c97a79043fed5e7
adudleyssf <adudley@softstackfactory.org>
2016-03-04 18:12:42 +0000

    finisished ionic lesson

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'ionic.service.core', "ionic.service.analytics", 'starter.controllers', 'RESTConnection', "TKServicesModule", "chart.js", "SSFAlerts", 'pascalprecht.translate', "tmh.dynamicLocale"])
+angular.module('starter', ['ionic','ionic.service.core',  "ionic.service.analytics", 'starter.controllers', 'RESTConnection', "TKServicesModule", "chart.js", "SSFAlerts", 'pascalprecht.translate', "tmh.dynamicLocale"])
 
 .run(["$ionicPlatform", "$window", "$state", "$ionicAnalytics", function($ionicPlatform, $window, $state, $ionicAnalytics) {
   $ionicPlatform.ready(function() {
